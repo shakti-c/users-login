c057914e604d3d1d0859026848f2cc8e15b64aea
shakti-c <shakti28021994@gmail.com>
2020-02-09 18:11:42 +0530

    Initital

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+require('./db/mongoose')
+const redisClient = require('./db/redis')
+
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
+})
+
+async function init () {
+  redisClient.setAsync('one', JSON.stringify({ json: true }))
+}
+
+init()
